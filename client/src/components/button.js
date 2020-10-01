d18bcdc038f73234bd2d22bdb063116a06f3ae54
BradHacker <harkerbd@gmail.com>
2020-10-01 14:58:38 -0400

    Update Button styling

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './button.scss';
 
-const Button = ({ variant, to, onClick, children, className, type }) =>
+const Button = ({ variant, to, onClick, children, className, type, icon, title }) =>
   to ? (
     <Link to={to} className={`button ${variant ? `button-${variant}` : ''} ${className || ''}`}>
       {children}
@@ -13,8 +13,10 @@ const Button = ({ variant, to, onClick, children, className, type }) =>
       type={type || 'button'}
       className={`button ${variant ? `button-${variant}` : ''} ${className || ''}`}
       onClick={onClick}
+      title={title}
     >
       {children}
+      {variant === 'icon' && <i className={`fa fa-${icon}`} />}
     </button>
   );
 
