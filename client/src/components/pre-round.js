84fede445529bdf6be6003bf37d50085a9eda623
BradHacker <harkerbd@gmail.com>
2020-10-01 14:59:30 -0400

    Add round duration to pre-round screen

--- client/src/components/pre-round.js
+++ client/src/components/pre-round.js
@@ -3,9 +3,10 @@ import './pre-round.scss';
 import React from 'react';
 import Button from './button';
 
-const PreRound = ({ competing, ready, onReady, roundNum }) => (
-  <div className="round-start-container">
-    <p className="headline2 round-title">Round {roundNum || '?'}</p>
+const PreRound = ({ competing, ready, onReady, fight }) => (
+  <div className="pre-round-container">
+    <p className="headline2 round-title">Round {fight.currentRound || '?'}</p>
+    <p className="headline4">Round Length: {parseInt(fight.roundDuration) / 60} minutes</p>
     <p className="headline5 ready-prompt">
       {competing ? 'Are you ready to start the next round?' : 'Waiting on players to get ready...'}
     </p>
