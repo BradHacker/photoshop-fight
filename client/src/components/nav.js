b0a8c7a4614f1bf704e8e07a9e221cd1dbbc8f9a
BradHacker <harkerbd@gmail.com>
2020-10-01 14:58:52 -0400

    Add dashboard button to nav

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
+import Button from '../components/button';
 
 import User from '../assets/user.png';
 import './nav.scss';
@@ -11,6 +12,9 @@ const Nav = ({ user, variant, children }) => (
       <img src={Logo} className="logo surface-1" alt="Logo" />
     </Link>
     {children}
+    <Button to="/dashboard" variant="text">
+      Dashboard
+    </Button>
     <img src={(user && user.image) || User} className="user-pfp" alt="Profile" />
   </nav>
 );
